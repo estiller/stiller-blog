c368e64ec59d177d4ca0965542bf6f0e2ad7bee9
Eran Stiller <git@stiller.blog>
2018-02-02 09:23:19 +0200

    Add alternate domains for certificate

--- haproxy/letsencrypt/docker-entrypoint-custom.sh
+++ haproxy/letsencrypt/docker-entrypoint-custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Obtain a certificate
-certbot certonly --standalone --preferred-challenges http --http-01-port 80 -d stiller.blog -d www.stiller.blog -n --agree-tos --email git@stiller.co.il
+certbot certonly --standalone --preferred-challenges http --http-01-port 80 -d stiller.blog -d www.stiller.blog -d stiller.co.il -d www.stiller.co.il -n --agree-tos --email git@stiller.co.il
 cat /etc/letsencrypt/live/stiller.blog/fullchain.pem /etc/letsencrypt/live/stiller.blog/privkey.pem > /etc/ssl/private/stiller.blog.pem
 chmod -R go-rwx /etc/ssl/private
 
